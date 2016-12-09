c9615f7d67b1b7fc22ac45f1d2a8efe8bba90ef0
Jessica <jkelly@urban.org>
2016-12-09 11:59:34 -0500

    more proj euler

--- proj_euler/src/main.rs
+++ proj_euler/src/main.rs
@@ -76,6 +76,9 @@ fn solve_problem(opt: Option<String>) {
     else if opt_val == "8" {
     	problem_8();
     }
+    else if opt_val == "9" {
+    	problem_9();
+    }
     else {
     	println!("not solved yet");
     }   
@@ -327,29 +330,8 @@ fn problem_7 () {
 	}	
 }
 
-// the number is:
-// 73167176531330624919225119674426574742355349194934
-// 96983520312774506326239578318016984801869478851843
-// 85861560789112949495459501737958331952853208805511
-// 12540698747158523863050715693290963295227443043557
-// 66896648950445244523161731856403098711121722383113
-// 62229893423380308135336276614282806444486645238749
-// 30358907296290491560440772390713810515859307960866
-// 70172427121883998797908792274921901699720888093776
-// 65727333001053367881220235421809751254540594752243
-// 52584907711670556013604839586446706324415722155397
-// 53697817977846174064955149290862569321978468622482
-// 83972241375657056057490261407972968652414535100474
-// 82166370484403199890008895243450658541227588666881
-// 16427171479924442928230863465674813919123162824586
-// 17866458359124566529476545682848912883142607690042
-// 24219022671055626321111109370544217506941658960408
-// 07198403850962455444362981230987879927244284909188
-// 84580156166097919133875499200524063689912560717606
-// 05886116467109405077541002256983155200055935729725
-// 71636269561882670428252483600823257530420752963450
-//
-// change this number to an array and slice it by 13, remove any slice that has a 0 in it.
+// the number is (below, manually made into vector):
+// change this number to an array and slice it by 13, remove any slice that has a 0 in it (ideally, but this is not implemented).
 // then do the products and find the slice that has the biggest one
 fn problem_8() {
 	println!("PROBLEM:Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?");
@@ -381,7 +363,7 @@ fn problem_8() {
 
 	for i in 0..n.len() - 12 {
 	//for i in 0..1 {
-		let thirteen = &n[i..i+13];
+		let thirteen = &n[i..i + 13];
 		//println!("i={}",i );
 		//println!("{:?}", thirteen );
 		let product: i64 = thirteen.iter().fold(1, |acc, &x| acc * x);
@@ -395,6 +377,32 @@ fn problem_8() {
 	println!("SOLUTION: The largest product is {}", largest_product);
 }
 
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
+// maybe i should use tuples? or a struct? 
+fn problem_9() {
+	println!("PROBLEM:");
+	// check if the tuple is a Pythagorean triple.
+	// is_pythagorean(a,b,c) -> bool should take three numbers and return true if a Pythagorean triple.
+	fn is_pythagorean(triple: (i64, i64, i64)) -> bool {
+		let (a,b,c) = triple;
+		let is = if a.pow(2) + b.pow(2) == c.pow(2) { true } else { false };
+		return is;
+	}
+	// test that function works as expected.
+	let mut test = (3,4,5);
+	let mut is = is_pythagorean(test);
+	println!("is {:?} Pythagorean? {}", test, is);
+	test = (5,7,8);
+	is = is_pythagorean(test);
+	println!("is {:?} Pythagorean? {}", test, is);
+
+    // generate tuples that add to 1000 where a < b < c . 
+    
+
+
+	println!("SOLUTION:");
+}
 
 
 
